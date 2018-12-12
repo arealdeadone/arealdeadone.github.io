bd0e6a1e0682bc670997872b88233a6ed660a9cb
Arvind <ari0997@gmail.com>
2018-12-12 20:42:15 +0530

    TrueSaviour Front End

---
+++ precache-manifest.a58132f65d26c1497c897dd5bc6bb470.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "31f92403b7a1ff2a73ed",
+    "url": "/static/js/main.31f92403.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "31f92403b7a1ff2a73ed",
+    "url": "/static/css/main.d48d335d.chunk.css"
+  },
+  {
+    "revision": "b7104af9056f20eb6e0c22c47601530d",
+    "url": "/index.html"
+  }
+];
